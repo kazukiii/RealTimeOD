3555505575014524b50557d531618a4f3f108176
kazuki <kazumatsu.dev28@gmail.com>
2022-08-26 17:31:41 -0700

    add arguments options

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@ import argparse
 from yolov5 import Yolov5
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--mode', '-m', default='webcam')
-parser.add_argument('--channel', '-c', default=0)
+parser.add_argument('-m', '--mode', default='webcam', help='select webcam or youtube, the default value is webcam')
+parser.add_argument('-c', '--channel', type=int, default=0, help='select the camera channel that you want to use')
+parser.add_argument('-a', '--arch', default='yolov5', help='select the model that you want to use, the default value is yolov5')
 
 
 if __name__ == "__main__":
